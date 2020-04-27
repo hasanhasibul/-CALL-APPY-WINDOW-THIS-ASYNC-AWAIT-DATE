b9edb8819da3d84dfffe0e72c88a54a58d86f74d
hasanhasibul <hasanhasibul395@gmail.com>
2020-04-28 02:36:43 +0600

    difference between bind, call and apply

--- callAply.js
+++ callAply.js
@@ -27,19 +27,47 @@ const student3 = {
     name : 'kalam',
     tutionFee : 25000,
 }
+
+
+// we can use apply function 
+
+console.log(student3);
+student.tutionFeeDue.call(student3,[2000]);
+console.log(student3.tutionFee);
+
+console.log(student2);
+student.tutionFeeDue.call(student2,[2000]);
+console.log(student2.tutionFee);
+
+// we can use call function 
+
+// console.log(student2);
+// student.tutionFeeDue.call(student2,2000); // here perameter separat with coma
+// console.log(student2.tutionFee);
+
+// console.log(student3);
+// student.tutionFeeDue.call(student3,2000);
+// console.log(student3.tutionFee);
+
+// normally we can use 
+
 // student.GetFullName();
 // student.GetMarks(20);
 // console.log(student.Marks);
 // student.tutionFeeDue(2000);
 // console.log(student.tutionFee);
 
-const newStudent = student.tutionFeeDue.bind(student2);
-newStudent(1000);
-console.log(student2.tutionFee);
 
-const student3due = student.tutionFeeDue.bind(student3);
-student3due(10000);
-console.log(student3.tutionFee)
+//we can use bind
+
+// const newStudent = student.tutionFeeDue.bind(student2);
+// newStudent(1000);
+// console.log(student2.tutionFee);
+
+// const student3due = student.tutionFeeDue.bind(student3);
+// student3due(10000);
+// console.log(student3.tutionFee)
+// console.log(student3)
 
 
 
