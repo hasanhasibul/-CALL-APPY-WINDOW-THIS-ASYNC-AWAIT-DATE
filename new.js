9a26a83743877f82faf230ba35d919acafc64d99
hasanhasibul <hasanhasibul395@gmail.com>
2020-05-01 01:47:56 +0600

    NEW KEYWORD, CLASS AND OBJECT DIFFERENCE

---
+++ new.js
@@ -0,0 +1,12 @@
+class Person{
+    constructor(first,last,salary){
+    this.firstName = first;
+    this.lastName = last;
+    this.salary = salary ;
+    }
+}
+
+const Person1 = new Person('Jhodhi','Mam',12000);
+console.log(Person1);
+const Person2 = new Person ('Jothi','Kundu',30000);
+console.log(Person2);
